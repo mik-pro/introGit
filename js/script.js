470c6528c1b435460eaf9afdcf26fea6adabca9d
Miras <miras.ismail.2003@mail.ru>
2024-10-30 10:18:04 +0300

    ismailmiras

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-console.log("javascript world");
-//бул script.js жазылган//
+//let name = "ismail"
+//if (true) {
+   // let name = "Miras"
+   // console.log("меным атым:" + name);//
+    
+//}//
+//console.log("жалпы аты" + name);//
+//const name = "miras"
+
+//name = "ismail"
+
+//console.log("меным атым" + name);
+let age =5
+let age2 =10
+let ismailmiras = false
+console.log(age+age2);
+
+
+
+
 
 
 
